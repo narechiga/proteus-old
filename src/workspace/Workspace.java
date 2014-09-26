c8a65ac8846bd5cfc16a3be3ff67fe63cfdeaa3a
Nikos Aréchiga <narechig@andrew.cmu.edu>
2014-09-25 21:04:32 -0700

    some thoughts

--- src/workspace/Workspace.java
+++ src/workspace/Workspace.java
@@ -26,4 +26,19 @@ public class Workspace {
 	
 // Manipulate a member
 	
+// what kind of operations do i need?
+// copy: copy an existing dLStructure into another cell, with another name--use clone method
+// 	copy newStructure = existingStructure
+// 		just call existingStructure.clone(), and add it to the hash set. Of the same
+// 		class as the one that was cloned.
+//
+// spawn: generate a new instance, overwriting any that may already exist
+// 	spawn RealVariable aVariable "a"
+// 		>Does this mean that I should create a new constructor, for everybody, so that they can
+// 		take string as input for their constructor?
+// 		> Probably the right thing to do is to omit the type declaration and call the parser on the string
+// 		that specified the structure, i.e.
+// 			spawn # aNumber # 34
+//
+//
 }
