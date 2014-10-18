c5ca1bea0bb0218e21e3dfdc792d3b1327a559b4
Nikos Aréchiga <narechig@ece.cmu.edu>
2014-10-17 20:42:48 -0700

    Added a DeclarationProgram, for a possibly more elegant way to handle variable declarations

---
+++ src/dl/syntax/DeclarationProgram.java
@@ -0,0 +1,36 @@
+package proteus.dl.syntax;
+
+import proteus.dl.semantics.*;
+import java.util.*;
+
+public class DeclarationProgram extends HybridProgram {
+
+	public DeclarationProgram( List<RealVariable> variables ) {
+		operator = new Operator( "declare-real", 1, false );
+		spawnArguments();
+		addArguments( variables );
+	}
+
+	public List<RealVariable> getDeclaredVariables() {
+		List<RealVariable> variables = new ArrayList<>();
+
+		for ( dLStructure argument : getArguments() ) {
+			variables.add( (RealVariable)argument );
+		}
+
+		return variables;
+	}
+
+	public DeclarationProgram clone() {
+		List<RealVariable> clonedVariables = new ArrayList<>();
+
+		for ( RealVariable variable : getDeclaredVariables() ) {
+			clonedVariables.add( variable.clone() );
+		}
+
+		return new DeclarationProgram( clonedVariables );
+	}
+
+
+}
+
